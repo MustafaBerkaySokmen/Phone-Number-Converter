deea92b648a648c07302ad9330f2415c68d9f161
Mustafa Berkay Sökmen <131465305+MustafaBerkaySokmen@users.noreply.github.com>
2025-01-04 16:34:42 +0300

    Add files via upload

---
+++ PhoneConverter.py
@@ -0,0 +1,26 @@
+def phone_converter(phone_alpha):
+    '''
+    Write your code inside this function.
+    '''
+    return 'change me' # replace this with the correct return value
+        
+
+def main():
+    '''
+    You can test your phone_converter via the function calls below.
+    These function calls are here only to help you test your function.
+    '''
+    print(phone_converter('555-GET-FOOD'))
+    print(phone_converter('310-EaT-MeaT'))
+    print(phone_converter('12E-456-789T'))
+    print(phone_converter('444-EAT-CHICKEN'))
+    print(phone_converter('444EATMEAT'))
+    print(phone_converter('090-EAT-MEAT'))   
+    
+
+    
+################################################################ 
+'''
+DO NOT EDIT BELOW THIS LINE
+'''
+main()
